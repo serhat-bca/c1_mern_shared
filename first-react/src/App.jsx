d031360954047afe7d5e38ada4103f51699dd254
serhat-bca <sersen@bergen.org>
2025-09-15 12:21:12 -0400

    props

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,3 +1,12 @@
+const ReactLogo = (props) => {
+  return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+      width={props.width}
+    />
+  );
+};
+
 const App = () => {
   const releaseYear = 2013;
   const devCompany = "Meta, Inc";
@@ -5,14 +14,20 @@ const App = () => {
   console.log("Developer Company: ", devCompany);
 
   return (
-    <div>
-      <h1>Hello React</h1>
-      <p>
-        React was developed by {devCompany} and was first released in{" "}
-        {releaseYear},{new Date().getFullYear() - releaseYear}
-        years ago.{" "}
-      </p>
-    </div>
+    <>
+      <div>
+        <h1>Hello React</h1>
+        <ReactLogo width="100" />
+        <ReactLogo width="75" />
+        <ReactLogo width="50" />
+        <p>
+          React was developed by {devCompany} and was first released in{" "}
+          {releaseYear},{new Date().getFullYear() - releaseYear}
+          years ago.{" "}
+        </p>
+      </div>
+      <p></p>
+    </>
   );
 };
 
