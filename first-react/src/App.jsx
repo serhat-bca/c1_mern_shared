cccfc60b340b10e84067e8df3652a40b1c97cbc2
serhat-bca <sersen@bergen.org>
2025-09-15 12:11:09 -0400

    component parts.

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,7 +1,17 @@
 const App = () => {
+  const releaseYear = 2013;
+  const devCompany = "Meta, Inc";
+  console.log("Release year: ", releaseYear);
+  console.log("Developer Company: ", devCompany);
+
   return (
     <div>
       <h1>Hello React</h1>
+      <p>
+        React was developed by {devCompany} and was first released in{" "}
+        {releaseYear},{new Date().getFullYear() - releaseYear}
+        years ago.{" "}
+      </p>
     </div>
   );
 };
