90c7bf49914590e57088d5a04e389f1373e5d11e
serhat-bca <sersen@bergen.org>
2025-09-18 12:01:13 -0400

    object child element.

--- contacts/src/App.jsx
+++ contacts/src/App.jsx
@@ -1,12 +1,14 @@
 const Contact = (props) => {
   return (
     <div>
+      {/* objects cannot be displayed as
+    a react child */}
       <br />
-      Name: {props.name}
+      Name: {props.contact.name}
       <br />
-      DOB: {props.dob}
+      DOB: {props.contact.dob}
       <br />
-      State: {props.state}
+      State: {props.contact.state}
     </div>
   );
 };
@@ -18,8 +20,8 @@ const App = () => {
   return (
     <div>
       <h1>Contacts</h1>
-      <Contact name="John Doe" dob="2/3/1997" state="NY" />
-      <Contact name="John Max" dob="12/10/2003" state="MA" />
+      <Contact contact={jd} />
+      <Contact contact={jm} />
     </div>
   );
 };
