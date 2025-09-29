60a1b67f0eaf9bb195475c3c1ce0228440c59397
serhat-bca <sersen@bergen.org>
2025-09-29 12:08:24 -0400

    init files with state counters

---
+++ counter_with_state/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const App = () => {
+  // let counter = 0;
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    console.log("Counter: ", counter);
+    // counter++;
+    // never mutate the state directly
+    // always use set method.
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => setCounter(counter - 1);
+
+  return (
+    <div>
+      <h3>Counter with React State</h3>
+      <p>Counter: {counter}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={() => setCounter(0)}>reset</button>
+      {/* minus button and a method for it */}
+      {/* +5 button and a method for it */}
+      {/* -5 button and a method for it */}
+      {/* a reset button to reset to 0 */}
+    </div>
+  );
+};
+
+export default App;
