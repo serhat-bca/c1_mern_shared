b92aa2a2e0ffd3d852d86b4737bf23c91567f73c
serhat-bca <sersen@bergen.org>
2025-10-09 12:09:53 -0400

    conditional rendering.

--- feedback/src/App.jsx
+++ feedback/src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
 
+const DiplayTracker = ({ tracker }) => {
+  // if (tracker.length === 0) {
+  //   return <p>No Feedback</p>;
+  // }
+  // return <p>Feedback History: {tracker.join("-")}</p>;
+  return tracker.length === 0 ? (
+    <p>No Feedback</p>
+  ) : (
+    <p>Feedback History: {tracker.join("-")}</p>
+  );
+};
+
 const App = () => {
-  const [recommend, setRecommend] = useState(0);
-  const [notRecommend, setNotRecommend] = useState(0);
   const [rec, setRec] = useState({ yes: 0, no: 0 });
+  const [tracker, setTracker] = useState([]);
 
   const recYes = () => {
     setRec({ ...rec, yes: rec.yes + 1 });
+    setTracker([...tracker, "Y"]);
+    console.log("Tracker: ", tracker);
+  };
+
+  const recNo = () => {
+    setRec({ ...rec, no: rec.no + 1 });
+    // setTracker([...tracker, "N"]);
+    setTracker(tracker.concat("N")); // instead of push use concat
+    console.log("Tracker: ", tracker);
   };
 
   return (
@@ -14,9 +34,10 @@ const App = () => {
       <h2>Feedback Application</h2>
       <p>Do you recommend this product?</p>
       <button onClick={recYes}>Yes</button>
-      <button onClick={() => setNotRecommend(notRecommend + 1)}>No</button>
+      <button onClick={recNo}>No</button>
       <p>Recommended: {rec.yes}</p>
       <p>Not Recommended: {rec.no}</p>
+      {tracker.length !== 0 && <DiplayTracker tracker={tracker} />}
     </div>
   );
 };
